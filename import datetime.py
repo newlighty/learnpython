31ccfa18788cb40c1ea1b463b5d57e669c49cecc
faramarz <37065807+newlighty@users.noreply.github.com>
2023-07-18 20:35:04 +0330

    Calculate the number of days until Mahsa's birthda

--- import datetime.py
+++ import datetime.py
@@ -14,3 +14,22 @@
 # formatted_datetime = jalali_datetime.strftime("%Y-%m-%B-%d %H:%M")
 
 # print(formatted_datetime)
+
+
+# how to calculate birthay of someone
+
+# from persiantools.jdatetime import JalaliDateTime
+
+# # Get the current Jalali date and time
+# jalali_datetime = JalaliDateTime.now()
+
+# formatted_datetime = jalali_datetime.strftime("%Y-%m-%B-%d %H:%M")
+
+# # Create a JalaliDateTime object for Mahsa's birthday
+# mahsa_birthday = JalaliDateTime(1402, 5, 2)
+
+# # Calculate the number of days until Mahsa's birthday
+# days_until_birthday = (mahsa_birthday - jalali_datetime).days
+
+# # Print the number of days until Mahsa's birthday
+# print("Days until Mahsa's birthday:", days_until_birthday, formatted_datetime)
