6d646a68a7ac709b6ec35d9bb40b6a2b946e4f40
faramarz <37065807+newlighty@users.noreply.github.com>
2023-07-18 12:33:24 +0330

    use formatted text in persian datetime

--- import datetime.py
+++ import datetime.py
@@ -1,3 +1,16 @@
-import datetime
+# import datetime
 
-current  = datetime.datetime.now()
+# current  = datetime.datetime.now()
+
+
+# this is how you can show formatted date in persian
+# from persiantools.jdatetime import JalaliDateTime
+
+
+# for run this use run in current interactive mode in debug
+# from persiantools.jdatetime import JalaliDateTime
+# jalali_datetime = JalaliDateTime.today()
+
+# formatted_datetime = jalali_datetime.strftime("%Y-%m-%B-%d %H:%M")
+
+# print(formatted_datetime)
