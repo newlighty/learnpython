e446394679ab06136e01c3f0061db492773dc095
faramarz <37065807+newlighty@users.noreply.github.com>
2023-07-25 16:47:54 +0330

    if example added

---
+++ if.py
@@ -0,0 +1,14 @@
+# lf i n python
+
+# answer=input(" do you like me ? ")
+
+# if answer == "yes" :
+#     print(" well it makes me happy thank you ")
+
+# answer=input(" do you like me ? ")
+
+# if answer == "yes" :
+#     print(" well it makes me happy thank you ")
+
+# if answer == "no" :
+#     print(" have nice day")
