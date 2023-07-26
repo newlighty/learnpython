e6d40fb2b4b4ae279e52c62dc542849ff5b2ebb3
faramarz <37065807+newlighty@users.noreply.github.com>
2023-07-26 16:46:36 +0330

    boolean

--- if.py
+++ if.py
@@ -46,3 +46,43 @@
 #     print(" your are good customer ")
 
 # hear you must convert variable from string to int so we use int or we get error
+
+# using else
+
+# valet = int(input("enter the value"))
+
+# if valet > 100 :
+
+#      print(" your are good customer ")
+
+# else:
+#     print("this your lukey day")
+
+# using boolean =========
+
+#  hear if you enter less 100 you get error that value is not defined because  it is boolean and only if it's true  it work
+
+# if valet > 100 :
+#     value = True
+
+# if value :
+
+#      print(" your are good customer ")
+
+
+# so to solve problem hear we first declare variable as true and than if meet conditions it change to True
+
+
+# value = False
+
+# valet = int(input("enter the value"))
+
+# if valet > 100 :
+#     value = True
+
+# if value :
+
+#      print(" your are good customer ")
+
+# else :
+#     print("you money is not enough!")
