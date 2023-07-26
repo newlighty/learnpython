320660034bcccc97baf9568b69e4fdb87ef2bea3
faramarz <37065807+newlighty@users.noreply.github.com>
2023-07-26 08:48:05 +0330

    some more if exp

--- if.py
+++ if.py
@@ -12,3 +12,29 @@
 
 # if answer == "no" :
 #     print(" have nice day")
+
+# ASK put condition that you must type only upper case
+
+# pythonLover = input("what is you favorite programming language ? type upper!")
+
+# if pythonLover.upper() == "PYTHON" :
+#     print("yes that's it")
+#     print("love django by the way")
+
+# print("its ok if you love other languages")
+
+# the last line print whether it true or false unintended print outside the condition just for showing
+
+# pythonLover = input("what is you favorite programming language ?")
+
+# if pythonLover.upper() == "PYTHON" :
+#     print("yes that's it")
+#     print("love django by the way")
+
+# replaced upper with capitalize
+
+# pythonLover = input("what is you favorit programing language ?")
+
+# if pythonLover.capitalize() == "Python" :
+#     print("yes that's it")
+#     print("love django by the way")
